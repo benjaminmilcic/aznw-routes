9f2708b88940a9eff107f007241805957e1922d9
Benjamin Milcic <benjamin.milcic@gmail.com>
2026-04-26 10:47:27 +0200

    homepage portfolio changed

--- src/app/pages/home/components/portfolio/portfolio.component.ts
+++ src/app/pages/home/components/portfolio/portfolio.component.ts
@@ -1,21 +1,19 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { HomeService } from '../../home.service';
-import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css'],
-  imports: [TranslateModule, RouterModule, CommonModule, MatIconModule],
+  imports: [TranslateModule, RouterModule, MatIconModule],
 })
 export class PortfolioComponent {
   constructor(
     public homeService: HomeService,
-    private router: Router,
-    public translateService: TranslateService
+    private router: Router
   ) {}
 
   closeDialog(dialog: HTMLDialogElement) {
