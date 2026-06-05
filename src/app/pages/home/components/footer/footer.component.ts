39f6917e559ba978fcc16b85aea90d670090fd48
Benjamin Milcic <benjamin.milcic@gmail.com>
2026-06-05 11:30:04 +0200

    footer changed

--- src/app/pages/home/components/footer/footer.component.ts
+++ src/app/pages/home/components/footer/footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.css'],
-    imports: [TranslateModule]
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  imports: [TranslateModule],
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+  currentYear = signal<string>('2026');
+
+  ngOnInit(): void {
+    const now = new Date();
+    this.currentYear.set(now.getFullYear().toString());
+  }
+}
